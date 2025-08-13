3e2070b35b0850c80f09da6df51242cf35308485
rizzamaegonzalespore <porerizzamae136@gmail.com>
2025-08-13 01:06:26 +0000

    fix line 4

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php
 $color = "red";
 echo "My car is " . $color . "<br>";
-echo "My house is " . $COLOR . "<br>";
+echo "My house is " . $color . "<br>";
 echo "My boat is " . $coLOR . "<br>";
 ?>
